978e4f62d5a32c2da588facc8e773c6c4daab82a
yanglei <clanherb@gmail.com>
2011-03-02 18:46:46 +0800

    发布v1.12，加入各地延迟的检测类：Nginx::RegionLatency。 使用独立脚本的方式，脚本在fast项目中。
    对metrics的格式做调整，多加入客户端IP。Ocollector本身不做处理。

--- ocollector.pl
+++ ocollector.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 # author:        yanglei@snda.com
-# last modified: 2011-02-26
+# last modified: 2011-03-0
 # description:   this script collects interesting data then send to some place for scrunity.
 
 use strict;
@@ -32,7 +32,7 @@ use Net::Address::IP::Local;
 use constant WIN32 => $^O eq 'MSWin32';
 use constant SUNOS => $^O eq 'solaris';
 
-our $VERSION = "1.11";
+our $VERSION = "1.12";
 $VERSION = eval $VERSION;
 
 
